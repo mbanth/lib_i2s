ff9d58263068f28adf096d389db9a18a4815a557
Michael Banther <michaelb@xmos.com>
2019-05-10 14:27:50 +0100

    Revert "Change i2s_frame_master to support 4 busses."
    This reverts commit 96951d61c437ea935f06c3c2a9950e288d9b18c9.

--- lib_i2s/src/i2s_frame_master_impl.h
+++ lib_i2s/src/i2s_frame_master_impl.h
@@ -6,74 +6,12 @@
 #include "i2s.h"
 #include "xassert.h"
 
-#define NUM_IO_PER_I2S_FRAME    8   //The number of input/outputs to 4b port over one I2S frame
-#define NUM_CHANS_PER_WIRE      4
-#define I2S_WIRES               1
-
-//-----------------------------------------------------------------------------
-// Description: Helper function to zip up output samples to 4b buffered ports
-//     Returns: none
-//      Inputs: samplesOut - array of input PCM samples
-//      Outputs: zipped_outs - array of 32b values to be output to buffered port
-//-----------------------------------------------------------------------------
-#pragma unsafe arrays
-static inline void zip_samplesOut_to_output_buffer_left(unsigned samplesOut[], unsigned zipped_outs[NUM_IO_PER_I2S_FRAME]){
-    unsigned long long temp64_outs[2];
-
-    /* Packs of 1 bit */
-    temp64_outs[0] = zip(samplesOut[0], samplesOut[2], 0);      // Interleave L1 & L2
-    temp64_outs[1] = zip(samplesOut[4], samplesOut[6], 0);      // Interleave L3 & L4
-    zipped_outs[0] = (unsigned int)(temp64_outs[0] >> 32);      // MSB of [L1,L2] into 32bits
-    zipped_outs[1] = (unsigned int)(temp64_outs[0] >>  0);      // LSB of [L1,L2] into 32bits
-    zipped_outs[2] = (unsigned int)(temp64_outs[1] >> 32);      // MSB of [L3,L4] into 32bits
-    zipped_outs[3] = (unsigned int)(temp64_outs[1] >>  0);      // LSB of [L3,L4] into 32bits
-
-    /* Packs of 2 bits */
-    temp64_outs[0] = zip(zipped_outs[0], zipped_outs[2], 1);    // Interleave MSB's of [L1,L2] with [L3,L4]
-    temp64_outs[1] = zip(zipped_outs[1], zipped_outs[3], 1);    // Interleave LSB's of [L1,L2] with [L3,L4]
-    zipped_outs[0] = (unsigned int)(temp64_outs[0] >> 32);      // MSBs of [L1,L2,L3,L4] into 32bits
-    zipped_outs[1] = (unsigned int)(temp64_outs[0]      );      // MSBs of [L1,L2,L3,L4] into 32bits
-    zipped_outs[2] = (unsigned int)(temp64_outs[1] >> 32);      // LSBs of [L1,L2,L3,L4] into 32bits
-    zipped_outs[3] = (unsigned int)(temp64_outs[1]      );      // LSBs of [L1,L2,L3,L4] into 32bits
-}
-
-//-----------------------------------------------------------------------------
-// Description: Helper function to zip up output samples to 4b buffered ports
-//     Returns: none
-//      Inputs: samplesOut - array of input PCM samples
-//      Outputs: zipped_outs - array of 32b values to be output to buffered port
-//-----------------------------------------------------------------------------
-#pragma unsafe arrays
-static inline void zip_samplesOut_to_output_buffer_right(unsigned samplesOut[], unsigned zipped_outs[NUM_IO_PER_I2S_FRAME]){
-    unsigned long long temp64_outs[2];
-
-    /* Packs of 1 bit */
-    temp64_outs[0] = zip(samplesOut[1], samplesOut[3], 0);      // Interleave R1 & R2
-    temp64_outs[1] = zip(samplesOut[5], samplesOut[7], 0);      // Interleave R3 & R4
-    zipped_outs[0] = (unsigned int)(temp64_outs[0] >> 32);      // MSB of [R1,R2] into 32bits
-    zipped_outs[1] = (unsigned int)(temp64_outs[0] >>  0);      // LSB of [R1,R2] into 32bits
-    zipped_outs[2] = (unsigned int)(temp64_outs[1] >> 32);      // MSB of [L3,L4] into 32bits
-    zipped_outs[3] = (unsigned int)(temp64_outs[1] >>  0);      // LSB of [L3,L4] into 32bits
-
-
-    /* Packs of 2 bits */
-    temp64_outs[0] = zip(zipped_outs[0], zipped_outs[2], 1);    // Interleave MSB's of [R1,R2] with [R3,R4]
-    temp64_outs[1] = zip(zipped_outs[1], zipped_outs[3], 1);    // Interleave LSB's of [R1,R2] with [R3,R4]
-    zipped_outs[4] = (unsigned int)(temp64_outs[0] >> 32);      // MSBs of [R1,R2,R3,R4] into 32bits
-    zipped_outs[5] = (unsigned int)(temp64_outs[0]      );      // MSBs of [R1,R2,R3,R4] into 32bits
-    zipped_outs[6] = (unsigned int)(temp64_outs[1] >> 32);      // LSBs of [R1,R2,R3,R4] into 32bits
-    zipped_outs[7] = (unsigned int)(temp64_outs[1]      );      // LSBs of [R1,R2,R3,R4] into 32bits
-}
-
 static void i2s_frame_init_ports(
         out buffered port:32 (&?p_dout)[num_out],
         static const size_t num_out,
         in buffered port:32 (&?p_din)[num_in],
         static const size_t num_in,
-        out port p_bclk0,
-        out port p_bclk1,
-        out port p_bclk2,
-        out port p_bclk3,
+        out port p_bclk,
         out buffered port:32 p_lrclk,
         clock bclk,
         in port p_mclk,
@@ -81,10 +19,7 @@ static void i2s_frame_init_ports(
 
     set_clock_on(bclk);
     configure_clock_src_divide(bclk, p_mclk, mclk_bclk_ratio >> 1);
-    configure_port_clock_output(p_bclk0, bclk);
-    configure_port_clock_output(p_bclk1, bclk);
-    configure_port_clock_output(p_bclk2, bclk);
-    configure_port_clock_output(p_bclk3, bclk);
+    configure_port_clock_output(p_bclk, bclk);
     configure_out_port(p_lrclk, bclk, 1);
     for (size_t i = 0; i < num_out; i++)
         configure_out_port(p_dout[i], bclk, 0);
@@ -98,10 +33,7 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
         static const size_t num_out,
         in buffered port:32 (&?p_din)[num_in],
         static const size_t num_in,
-        out port p_bclk0,
-        out port p_bclk1,
-        out port p_bclk2,
-        out port p_bclk3,
+        out port p_bclk,
         clock bclk,
         out buffered port:32 p_lrclk,
         unsigned ratio,
@@ -109,12 +41,11 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
 
     int32_t in_samps[16]; //Workaround: should be (num_in << 1) but compiler thinks that isn't const,
     int32_t out_samps[16];//so setting to 16 which should be big enough for most cases
-    unsigned int zipped_outs[I2S_WIRES][NUM_IO_PER_I2S_FRAME];
 
     // Since #pragma unsafe arrays is used need to ensure array won't overflow.
     assert((num_in << 1) <= 16);
 
-    unsigned lr_mask = 0xF0000000; // Was lr_mask = 0
+    unsigned lr_mask = 0;
 
     for (size_t i=0;i<num_out;i++)
         clearbuf(p_dout[i]);
@@ -131,12 +62,8 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
     }
 
 #pragma loop unroll
-    for (size_t i=0; i<num_out; i++){
-        zip_samplesOut_to_output_buffer_left((unsigned int*)&out_samps[i * NUM_CHANS_PER_WIRE], zipped_outs[i]); // Turn our samples into port vals
-        p_dout[i] @ (1 + offset) <: bitrev(zipped_outs[i][0]);
-        p_dout[i] <: bitrev(zipped_outs[i][1]);
-        p_dout[i] <: bitrev(zipped_outs[i][2]);
-        p_dout[i] <: bitrev(zipped_outs[i][3]);
+    for (size_t i=0, idx=0; i<num_out; i++, idx+=2){
+        p_dout[i] @ (1 + offset) <: bitrev(out_samps[idx]);
     }
 
     p_lrclk @ 1 <: lr_mask;
@@ -146,10 +73,7 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
     //And pre-load the odds (1,3,5..)
 #pragma loop unroll
     for (size_t i=0, idx=1; i<num_out; i++, idx+=2){
-        p_dout[i] <: bitrev(zipped_outs[i][0 + 4]);
-        p_dout[i] <: bitrev(zipped_outs[i][1 + 4]);
-        p_dout[i] <: bitrev(zipped_outs[i][2 + 4]);
-        p_dout[i] <: bitrev(zipped_outs[i][3 + 4]);
+        p_dout[i] <: bitrev(out_samps[idx]);
     }
 
     lr_mask = ~lr_mask;
@@ -168,12 +92,8 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
 
             //Output i2s evens (0,2,4..)
 #pragma loop unroll
-            for (size_t i=0; i<num_out; i++){
-                zip_samplesOut_to_output_buffer_left((unsigned int*)&out_samps[i * NUM_CHANS_PER_WIRE], zipped_outs[i]); // Turn our samples into port vals
-                p_dout[i] @ (1 + offset) <: bitrev(zipped_outs[i][0]);
-                p_dout[i] <: bitrev(zipped_outs[i][1]);
-                p_dout[i] <: bitrev(zipped_outs[i][2]);
-                p_dout[i] <: bitrev(zipped_outs[i][3]);
+            for (size_t i=0, idx=0; i<num_out; i++, idx+=2){
+                p_dout[i] <: bitrev(out_samps[idx]);
             }
         }
 
@@ -191,11 +111,8 @@ static i2s_restart_t i2s_frame_ratio_n(client i2s_frame_callback_if i2s_i,
         if (restart == I2S_NO_RESTART) {
             //Output i2s odds (1,3,5..)
 #pragma loop unroll
-            for (size_t i=0; i<num_out; i++){
-                p_dout[i] <: bitrev(zipped_outs[i][0 + 4]);
-                p_dout[i] <: bitrev(zipped_outs[i][1 + 4]);
-                p_dout[i] <: bitrev(zipped_outs[i][2 + 4]);
-                p_dout[i] <: bitrev(zipped_outs[i][3 + 4]);
+            for (size_t i=0, idx=1; i<num_out; i++, idx+=2){
+                p_dout[i] <: bitrev(out_samps[idx]);
             }
 
             lr_mask = ~lr_mask;
@@ -232,15 +149,10 @@ static void i2s_frame_master0(client i2s_frame_callback_if i2s_i,
                 static const size_t num_out,
                 in buffered port:32 (&?p_din)[num_in],
                 static const size_t num_in,
-                out port p_bclk0,
-                out port p_bclk1,
-                out port p_bclk2,
-                out port p_bclk3,
+                out port p_bclk,
                 out buffered port:32 p_lrclk,
                 in port p_mclk,
                 clock bclk){
-    xassert( num_in ==0 );
-
     while(1){
         i2s_config_t config;
         unsigned mclk_bclk_ratio_log2;
@@ -253,12 +165,12 @@ static void i2s_frame_master0(client i2s_frame_callback_if i2s_i,
         mclk_bclk_ratio_log2 = clz(bitrev(config.mclk_bclk_ratio));
 
         //This ensures that the port time on all the ports is at 0
-        i2s_frame_init_ports(p_dout, num_out, p_din, num_in, p_bclk0, p_bclk1, p_bclk2, p_bclk3, p_lrclk, bclk,
+        i2s_frame_init_ports(p_dout, num_out, p_din, num_in, p_bclk, p_lrclk, bclk,
             p_mclk, config.mclk_bclk_ratio);
 
         i2s_restart_t restart =
           i2s_frame_ratio_n(i2s_i, p_dout, num_out, p_din,
-                      num_in, p_bclk0, p_bclk1, p_bclk2, p_bclk3, bclk, p_lrclk,
+                      num_in, p_bclk, bclk, p_lrclk,
                       mclk_bclk_ratio_log2, config.mode);
 
         if (restart == I2S_SHUTDOWN)
@@ -273,15 +185,12 @@ inline void i2s_frame_master1(client interface i2s_frame_callback_if i,
         static const size_t num_i2s_out,
         in buffered port:32 i2s_din[num_i2s_in],
         static const size_t num_i2s_in,
-        out port i2s_bclk0,
-        out port i2s_bclk1,
-        out port i2s_bclk2,
-        out port i2s_bclk3,
+        out port i2s_bclk,
         out buffered port:32 i2s_lrclk,
         in port p_mclk,
         clock clk_bclk) {
     i2s_frame_master0(i, i2s_dout, num_i2s_out, i2s_din, num_i2s_in,
-                i2s_bclk0, i2s_bclk1, i2s_bclk2, i2s_bclk3, i2s_lrclk, p_mclk, clk_bclk);
+                i2s_bclk, i2s_lrclk, p_mclk, clk_bclk);
 }
 
 #endif // __XS2A__
